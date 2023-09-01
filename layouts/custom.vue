8ce81fc2bdb33d65df6b4b3883590cf3646213e7
barbapapazes <e.soubiran25@gmail.com>
2023-09-01 15:39:34 +0200

    chore: initial commit

---
+++ layouts/custom.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const { data } = useFetch('https://ungh.cc/repos/unjs/nitro')
+</script>
+
+<template>
+  <div>
+    {{ data }}
+    <p>Custom Layout</p>
+    <hr/>
+    <slot />
+  </div>
+</template>
